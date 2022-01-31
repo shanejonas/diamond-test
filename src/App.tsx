640cc63f371d0bb5b42e0e2795d68b67bb852542
Shane Jonas <jonas.shane@gmail.com>
2022-01-30 17:11:29 -0800

    Fixed typo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
-import diamondLoupeFactArtifact from './artifacts/contracts/facets/DiamondLoupeFacet.sol/DiamondLoupeFacet.json';
+import diamondLoupeFacetArtifact from './artifacts/contracts/facets/DiamondLoupeFacet.sol/DiamondLoupeFacet.json';
 
 // Diamond Contract Address
 const CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
@@ -10,7 +10,7 @@ const App = () => {
   const [provider, setProvider] = useState<any>();
   const main = async () => {
     let provider = new ethers.providers.Web3Provider((window as any).ethereum);
-    let contract = new ethers.Contract(CONTRACT_ADDRESS, diamondLoupeFactArtifact.abi, provider);
+    let contract = new ethers.Contract(CONTRACT_ADDRESS, diamondLoupeFacetArtifact.abi, provider);
     const facets = await contract.facets();
     console.log('facets', facets);
   }
